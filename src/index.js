f71475b45c4504a8fd90c6a2ac6493b61c399942
Jason Zhou <jzhou35@buffalo.edu>
2021-01-13 13:47:38 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querySelector('form#create-task-form');
+  submit.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const task = document.querySelector('input[type="text"]').value;
+    const ulList = document.querySelector('#tasks');
+    const liElement = document.createElement('li');
+    liElement.textContent = task;
+    ulList.append(liElement);
+    
+    e.target.reset();
+  });
+
+  // const taskElem = document.querySelector('li');
+  // const ul = document.querySelector('ul#tasks');
+  // function deleteTask(task){
+  //   const liList = ul.querySelectorAll('li');
+  //   liList.remove()
+  //   }
+
+  // deleteTask(taskElem);
 });
